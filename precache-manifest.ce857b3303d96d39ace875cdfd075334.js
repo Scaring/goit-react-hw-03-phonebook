a1e97d5307d5a863b6216d5cc746b42e01fa1587
Dmytro Kuschevyy <scaring22@gmail.com>
2020-05-11 21:55:15 +0300

    Updates

---
+++ precache-manifest.ce857b3303d96d39ace875cdfd075334.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a3f27c87a5754f69fe0f93816b6d2bc",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "c558472a7ff12374bfd0",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.da6ae670.chunk.css"
+  },
+  {
+    "revision": "b41abd3dd0c063860c6d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.53bd274f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.53bd274f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c558472a7ff12374bfd0",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.1dd6f107.chunk.js"
+  },
+  {
+    "revision": "653c99886214260df0f5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.067fb2b4.js"
+  }
+]);
